87e2522161e767def2cf175993e021f9ba3f9d47
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-05-12 10:03:57 -0300

    add props to AbButton

--- src/components/AbButton/index.tsx
+++ src/components/AbButton/index.tsx
@@ -1,21 +1,39 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const StyledButton = styled.button`
-    color: #FFF;
-    background-color: #EB9B00;
+export interface AbButtonProps {
+    text?: string;
+    btnStyle?: 'primary' | 'secondary';
+    onClick?: () => void;
+}
+
+// button with different colors depending on "btnStyle" prop
+const StyledButton = styled.button<AbButtonProps>`
+    color: ${(props: AbButtonProps) => props.btnStyle === 'primary' ? '#FFF' : '#EB9B00'};;
+    background-color: ${(props: AbButtonProps) => props.btnStyle === 'primary' ? '#EB9B00' : '#FFF'};
     padding: 16px;
     border: 2px solid #EB9B00;
     font-size: 20px;
-    cursor: pointer;
-    &:hover {
-        background-color: #B87900;
-        border: 2px solid #B87900;
-    }
+    cursor: pointer;    
+    ${(props: AbButtonProps) => props.btnStyle === 'primary' 
+        ? css`
+            &:hover {
+                background-color: #B87900;
+                border: 2px solid #B87900;
+            }
+        `
+        : css`
+            &:hover {                
+                color: #B87900;
+                background-color: #FFF;
+                border: 2px solid #B87900;
+            }        
+        `
+    };
 `;
 
-export const AbButton = () => {
+export const AbButton = ({ text, onClick, btnStyle='primary' }: AbButtonProps) => {
     return (
-        <StyledButton>Clique aqui</StyledButton>
+        <StyledButton onClick={onClick} btnStyle={btnStyle}>{text}</StyledButton>
     )
 }
--- stories/AbButton.stories.tsx
+++ stories/AbButton.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react"
-import { AbButton } from "../src"
+import { AbButton, AbButtonProps } from "../src"
 
 // object to display the component in Storybook's dashboard
 export default {
@@ -8,10 +8,22 @@ export default {
     component: AbButton
 } as ComponentMeta<typeof AbButton>
 
-//--- creating the story
+//--- creating the stories
 
-// how to render the component for a story
-const Template: ComponentStory<typeof AbButton> = () => <AbButton />
+// model to render the component for a story, also passing its props (args)
+const Template: ComponentStory<typeof AbButton> = (args) => <AbButton {...args} />
 
-// create a "Primario" story with no arguments (props)
-export const Primario = Template.bind({})
+// create a "Primario" story
+export const Primario = Template.bind({});
+
+// set Primario args
+Primario.args = {
+    text: 'Primary button'
+} as AbButtonProps;
+
+// create another story with different args
+export const Secundario = Template.bind({});
+Secundario.args = {
+    text: 'Secondary button',
+    btnStyle: 'secondary'
+} as AbButtonProps
